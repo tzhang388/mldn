81a05588ef8ee39fbb8fe558d0e619db65b36f17
tzhang388 <tzhang388@gmail.com>
2017-01-16 00:27:11 -0500

    test again

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello
 {
 	public static void main(String args[]){
-		System.out.println("Hello world");
+		System.out.println("Hello GitHub");
 		System.out.println("hello, Git");
 	}
 }
